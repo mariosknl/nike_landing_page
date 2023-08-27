8d52748a9d4b4d85e6490e9ef73b4f18c4b0e77c
Marios Kanellopoulos <mariosknl@outlook.com>
2023-08-27 09:15:34 +0300

    finalizing project

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,4 +1,31 @@
+import ReviewCard from "../components/ReviewCard";
+import { reviews } from "../constants";
+
 const CustomerReviews = () => {
-	return <div>CustomerReviews</div>;
+	return (
+		<section className="max-container">
+			<h3 className="font-palanquin text-center text-4xl font-bold">
+				What Our
+				<span className="text-coral-red"> Customers </span>
+				Say?
+			</h3>
+			<p className="info-text mt-4 max-w-lg text-center">
+				Hear genuine stories from our satisfied customers about their
+				exceptional experience with us.
+			</p>
+
+			<div className="mt-24 flex flex-1 justify-evenly max-lg:flex-col gap-14">
+				{reviews.map((review) => (
+					<ReviewCard
+						key={review.customerName}
+						imgURL={review.imgURL}
+						customerName={review.customerName}
+						rating={review.rating}
+						feedback={review.feedback}
+					/>
+				))}
+			</div>
+		</section>
+	);
 };
 export default CustomerReviews;
